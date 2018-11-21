f0381cce75e8a3581a0bd8f0c3d7b28c83162adc
fidgetspinnerkid <35818373+fidgetspinnerkid@users.noreply.github.com>
2018-11-21 14:27:22 -0500

    added some pictures and modified code
    Let the code draw bozes around multiple faces

--- face_recog_test.py
+++ face_recog_test.py
@@ -2,23 +2,25 @@ import face_recognition
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+
 fig, ax = plt.subplots(1)
 
-image = face_recognition.load_image_file("Donald_Trump.jpg")
+image = face_recognition.load_image_file("OldPerson.jpg")
 
 plt.imshow(image)
 
 face_locations = face_recognition.face_locations(
     image)  # the location of the face in a list of tuples like [ (top, right, bottom, left) ]
 
-x, y, width, height = (face_locations[0][3],  # the x coord
-                       face_locations[0][2],  # the y coord
-                       face_locations[0][1] - face_locations[0][3],  # the width
-                       face_locations[0][0] - face_locations[0][2])  # the height
+for face in face_locations:
+    x, y, width, height = (face[3],  # the x coord
+                           face[2],  # the y coord
+                           face[1] - face[3],  # the width
+                           face[0] - face[2])  # the height
 
-rect = patches.Rectangle((x, y), width, height,
-                         linewidth=1, edgecolor='r',
-                         facecolor='none')  # draw a rectangle around the face
-ax.add_patch(rect)
+    rect = patches.Rectangle((x, y), width, height,
+                             linewidth=1, edgecolor='r',
+                             facecolor='none')  # draw a rectangle around the face
+    ax.add_patch(rect)
 
 plt.show()  # show the image with the rectangle
