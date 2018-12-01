f69b213f475754587734f288bf535f13f2d6d68e
John <johnyin596@gmail.com>
2018-12-01 20:45:24 +0000

    pushed pictures

--- face_recog_live.py
+++ face_recog_live.py
@@ -1,18 +1,32 @@
 import face_recognition
 import cv2
-
+import numpy as np
 video_capture = cv2.VideoCapture(0)
 
-# sample pic of me
+# sample pic of me TODO Change Face Encoding method: Identification faulty (racist)
+
 john_image = face_recognition.load_image_file("john.jpg")
 john_face_encoding = face_recognition.face_encodings(john_image)[0]
+
+shaq_image = face_recognition.load_image_file("Shaq2.png")
+shaq_face_encoding = face_recognition.face_encodings(shaq_image)[0]
+
+trump_image = face_recognition.load_image_file("Donald_Trump.jpg")
+trump_face_encoding = face_recognition.face_encodings(trump_image)[0]
+
+
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     john_face_encoding,
+    shaq_face_encoding,
+    trump_face_encoding,
+
 ]
 known_face_names = [
     "John Yin",
+    "Shaq",
+    "Donald Trump"
 ]
 
 while True:
@@ -22,7 +36,7 @@ while True:
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_frame = frame[:, :, ::-1]
 
-    # Find all the faces and face enqcodings in the frame of video
+    # Find all the faces and face encodings in the frame of video
     face_locations = face_recognition.face_locations(rgb_frame)
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
@@ -30,13 +44,14 @@ while True:
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-
+        distances = face_recognition.face_distance(known_face_encodings, face_encoding)
         name = "Unknown"
+
 
         # If a match was found in known_face_encodings, just use the first one.
         if True in matches:
-            first_match_index = matches.index(True)
-            name = known_face_names[first_match_index]
+            best_match = int(np.argmin([abs(x) for x in distances]))
+            name = known_face_names[best_match]
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -45,6 +60,8 @@ while True:
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+
 
     # Display the resulting image
     cv2.imshow('Video', frame)
